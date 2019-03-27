2873f8317b01b91460b45773125e7708e06599f4
Andrew Stoycos <astoycos@bu.edu>
2019-03-27 14:55:49 -0400

    Adding Command Line and Multigraph support

--- moleculeDB/src/com/company/Main.java
+++ moleculeDB/src/com/company/Main.java
@@ -2,20 +2,50 @@ package com.company;
 
 //import com.company.database;
 
+import java.util.Scanner;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         //try {
         //String filename = "water.txt";
+        System.out.println("Welcome to MoleculeDB");
+        System.out.println("This program stores chemical molecules, enter a command to continue \n");
+
         database DB = new database();
+
         DB.addCompound("water.txt");
         DB.addCompound("ammonia.txt");
-        DB.addCompound("carbon_dioxide.txt");
-        DB.findCompound("acetylene.txt");
-        DB.findCompound("water.txt");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input;
+
+        input = scanner.nextLine();
+
+        while(!input.isEmpty()) {
+            //System.out.println(input);
+            String[] input_splitted = input.trim().split("\\s+");
+            if (input_splitted[1].equals( "-addMolecule")){
+                DB.addCompound(input_splitted[2]);
+            }else if (input_splitted[1].equals("-findMolecule")){
+                DB.findCompound(input_splitted[2]);
+            }
+
+            // DB.addCompound("carbon_dioxide.txt");
+            // DB.findCompound("acetylene.txt");
+            // DB.findCompound("water.txt");
+
+            input = scanner.nextLine();
+
+        }
+
+        //database DB = new database();
         //} catch() {
-            //e.printStackTrace();
+        //e.printStackTrace();
         //}
+
     }
 }
--- moleculeDB/src/com/company/database.java
+++ moleculeDB/src/com/company/database.java
@@ -1,13 +1,20 @@
 package com.company;
+
+import org.jgrapht.*;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.Multigraph;
+
 import java.util.HashMap;
 import java.util.Vector;
 import java.io.*;
 
 public class database {
-
+    //Field: HashMap with key as vector of the vertices/structure and value as molecule name(data)
+    HashMap<Graph<String,DefaultEdge>,String> data;
     //Constructor: make database by making new HashMap
+
     public database() {
-        data = new HashMap<>();
+        data = new HashMap<Graph<String,DefaultEdge>,String>();
 
     }
 
@@ -16,6 +23,7 @@ public class database {
     //Read all other lines and store as vector (to be used as key)
     public void addCompound(String TextFile) {
         String line = null;
+        Graph<String, DefaultEdge> key2 = new Multigraph<>(DefaultEdge.class);
         Vector<String> key = new Vector<>();
         String value = new String();
         int count = 0;
@@ -24,12 +32,20 @@ public class database {
             while ((line = read.readLine())!= null) {
                 if (count == 0) {
                     value = line;
-                } else {
-                    key.addElement(line);
+                } else if(count > 1){
+                    key.addElement(line + (count - 2));
+                    if(line.length() == 1){
+                        key2.addVertex(line + (count - 2));
+                    }else{
+                        String[] edge = line.split(" ");
+                        key2.addEdge(key.get(Integer.parseInt(edge[0])),key.get(Integer.parseInt(edge[1])));
+                    }
+
                 }
                 count += 1;
             }
-            data.put(key, value);
+            data.put(key2, value);
+            System.out.println(key2.toString());
             System.out.println("Molecule " + value+ " added");
         } catch (FileNotFoundException ex) {
             System.out.println("File not found");
@@ -66,5 +82,4 @@ public class database {
     }
 
     //Field: HashMap with key as vector of the vertices/structure and value as molecule name(data)
-    private HashMap<Vector<String>, String> data;
 }
